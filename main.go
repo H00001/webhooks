72e57129a24cd1433a9cf0a9d589946869a89f9b
server.auto@ <server.auto@gunplan.top>
2020-02-27 09:20:37 +0800

    $(date)

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+	"os/exec"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/merge", merge)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func merge(c echo.Context) error {
+	exec.Command("bash -c","exec.sh")
+	return c.JSON(http.StatusOK, map[string]interface{}{"code":0,"message":"succeed"})
+}
